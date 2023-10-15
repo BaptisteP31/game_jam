9ff7f9a420b1db3326668f909c882a6803e8fcfd
Baptsite Paqueriaud <baptiste.paqueriaud@etu.univ-amu.fr>
2023-10-15 08:35:30 +0200

    mineur

--- sceneCredit.gd
+++ sceneCredit.gd
@@ -4,6 +4,4 @@ func _process(delta):
 	if Input.is_action_just_released("X"):
 		# animation bouton
 		get_tree().change_scene_to_file("res://sceneMenu.tscn")
-	if Input.is_action_just_released("B"):
-		#get_tree().quit()
-		get_tree().change_scene_to_file("res://sceneMenu.tscn")
+
