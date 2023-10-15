ae561f3149d70d8393e27d6d71139d2cc8578382
Baptsite Paqueriaud <baptiste.paqueriaud@etu.univ-amu.fr>
2023-10-15 11:45:27 +0200

    dsqeijgtesjù*k*ok

--- CharacterBody2D.gd
+++ CharacterBody2D.gd
@@ -43,11 +43,11 @@ func _physics_process(delta):
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
-	if direction:
-		velocity.x = direction * SPEED
-	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
+#	var direction = Input.get_axis("ui_left", "ui_right")
+#	if direction:
+#		velocity.x = direction * SPEED
+#	else:
+#		velocity.x = move_toward(velocity.x, 0, SPEED)
 		
 	if is_on_wall():
 		#get_tree().quit()
---
+++ pont_1.gd
@@ -0,0 +1,18 @@
+extends StaticBody2D
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta):
+	var parent = get_parent()
+	position = Vector2(position[0]- parent.speed, position[1])
+	
+	var sprite_size = 9600
+	
+	if position.x <= - (sprite_size):
+		# Réinitialise la position du sprite à la limite droite de l'écran
+		position.x = 9600
