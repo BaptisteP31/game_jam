4ad4e2a45e5e3c76d5af0c78f2668afc92430342
Keskas Yacine <yacine.keskas@etu.univ-amu.fr>
2023-10-16 15:46:51 +0200

    Jeu complet et fonctionnel

--- pont.gd
+++ pont.gd
@@ -1,11 +1,33 @@
 extends Node2D
 
-var speed:int = 5
-# Called when the node enters the scene tree for the first time.
+var speed:int = 10
+var Obstacles = []
+var obs1 = preload("res://obstacle_1.tscn")
+var obs2 = preload("res://obstacle_2.tscn")
+var obs3 = preload("res://obstacle_3.tscn")
+var obs4 = preload("res://obstacle_4.tscn")
+var obs5 = preload("res://obstacle_5.tscn")
+var obs6 = preload("res://obstacle_6.tscn")
+var obs7 = preload("res://obstacle_7.tscn")
+var obs8 = preload("res://obstacle_8.tscn")
+
 func _ready():
 	pass # Replace with function body.
 
-
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta):
-	pass
+	if Obstacles.size()<64:
+		Obstacles.append(obs1)
+		Obstacles.append(obs2)
+		Obstacles.append(obs3)
+		Obstacles.append(obs4)
+		Obstacles.append(obs5)
+		Obstacles.append(obs6)
+		Obstacles.append(obs7)
+		Obstacles.append(obs8)
+
+func _on_timer_timeout():
+	var nb_alea = randi_range(0, Obstacles.size()-1)
+	var new_obstacle = Obstacles[nb_alea].instantiate()
+	
+	add_child(new_obstacle)
