caae18e9efd4f652f2137347f4a8e18a228f0195
Keskas Yacine <yacine.keskas@etu.univ-amu.fr>
2023-10-18 21:34:53 +0200

    Bon courage à tous

--- main.gd
+++ main.gd
@@ -3,16 +3,14 @@ extends Node2D
 var viewWidth = ProjectSettings.get_setting("display/window/size/viewport_width")
 var viewHeight = ProjectSettings.get_setting("display/window/size/viewport_height")
 
-
-
 var mainAnchor = Vector2(147, 589)
 
 func _ready():
-	pass
+	Engine.get_frames_per_second()
 
 func _process(delta):
-	#append tout les obstacles dans le tableau
-	pass
+	print(Engine.get_frames_per_second())
+	
 
 func _ontimer_timeout():
 	pass
